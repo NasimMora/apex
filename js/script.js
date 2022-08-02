d39c0fc399122c7db3e131a6b8c054626f9462c1
NasimMora <brs.team.dota@gmail.com>
2022-08-02 14:56:23 +0400

    f

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const burgerBtn = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+burgerBtn.addEventListener('click', () => {
+	menu.classList.toggle('active')
+})
